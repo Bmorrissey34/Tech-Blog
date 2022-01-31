2503978f4c5e5d4ff360cc268335a1748a67df17
Bmorrissey34 <88950762+Bmorrissey34@users.noreply.github.com>
2022-01-31 17:29:28 -0500

    fixing heroku

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -33,24 +33,5 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-// router.delete('/:id', withAuth, async (req, res) => {
-//   try {
-//     const commentData = await Comment.destroy({
-//       where: {
-//         id: req.params.id,
-//         user_id: req.session.user_id,
-//       },
-//     });
-
-//     if (!commentData) {
-//       res.status(404).json({ message: 'No comment found with this id!' });
-//       return;
-//     }
-
-//     res.status(200).json(commentData);
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
 
 module.exports = router;
