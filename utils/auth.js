7c981893ad57d41c11dd46a6e537349ddafc30ef
Bmorrissey34 <88950762+Bmorrissey34@users.noreply.github.com>
2022-01-31 17:00:46 -0500

    pushing to heroku

--- utils/auth.js
+++ utils/auth.js
@@ -1,11 +1,8 @@
 const withAuth = (req, res, next) => {
-  // If the user is not logged in, redirect the user to the login page
-  // This is directly from the `/gallery/:id` and `/painting/:id` routes
-  if (!req.session.loggedIn) {
+  // If the user is not logged in, redirect the request to the login route
+  if (!req.session.logged_in) {
     res.redirect('/login');
   } else {
-    // If the user is logged in, execute the route function that will allow them to view the gallery
-    // We call next() if the user is authenticated
     next();
   }
 };
